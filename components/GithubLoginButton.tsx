a94b20c83ff898e9116d819b5000dda4922a3586
apaul02 <captainankan@gmail.com>
2025-07-04 02:38:16 +0530

    fix build

--- components/GithubLoginButton.tsx
+++ components/GithubLoginButton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { useState } from "react";
 import { motion } from "motion/react"
